754083be7884a0605841d5d68b40e37335be47f9
briancasio <113497684+briancasio@users.noreply.github.com>
2023-10-23 12:02:47 -0600

    Created .class file and printSum  method

--- Java/Class/MethodEx.java
+++ Java/Class/MethodEx.java
@@ -3,7 +3,19 @@ public class MethodEx {
     public static void main(String[] args) {
         int x = 5;
         int y = 2;
+
+        //call the method:
+        printSum(x,y);
+
     }
 
+    //Create a method that will take two integer as parameters
+    //The method should print the sum of two numbers
+    public static void printSum(int x, int y){
+    
+    int result = x + y;
+    System.out.println(result);
+    
+    }
 
 }
