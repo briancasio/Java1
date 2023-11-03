fc1cce3cd7442f1cc3f109c4ce3d11aba444493a
briancasio <113497684+briancasio@users.noreply.github.com>
2023-11-03 15:47:05 -0600

    Create addAccent method

---
+++ Java/Extra/Concatenator.java
@@ -0,0 +1,15 @@
+public class Concatenator{
+    public static void addAccent(String str) {
+        
+        String[] arrStr = str.split(" ");
+
+        for (int i = 0; i < arrStr.length; i++){
+        
+            System.out.println(arrStr[i] + " pft");
+
+
+        }
+
+    }
+    
+}
